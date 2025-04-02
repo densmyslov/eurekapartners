c23dd28db4888d10c66cbfb1360e25594b9e8ad0
densmyslov <denis@bergena.io>
2025-04-02 09:10:25 +0200

    delete COI

---
+++ admin_functions.py
@@ -0,0 +1,89 @@
+import requests
+import streamlit as st
+import pandas as pd
+from io import BytesIO
+from time import sleep
+import boto3
+import json
+
+S3_ACCESS_KEY = st.secrets["s3"]["S3_ACCESS_KEY"]
+S3_SECRET_KEY = st.secrets["s3"]["S3_SECRET_KEY"]
+S3_CLIENT = boto3.client('s3',
+            aws_access_key_id = S3_ACCESS_KEY,
+            aws_secret_access_key = S3_SECRET_KEY)
+BUCKET_NAME = st.secrets["s3"]["BUCKET_NAME"]
+COI_TABLE_NAME = st.secrets["s3"]["COI_TABLE_NAME"]
+
+def add_new_coi(full_name, email, initial_tokens, initial_price, access_on):
+    """
+    Adds a new COI to the database.
+
+    Parameters:
+    - full_name (str): Full name of the COI.
+    - email (str): Email address of the COI.
+    - initial_tokens (int): Initial token balance for the COI.
+    - initial_price (float): Initial token price for the COI.
+
+    """
+    cognito_jwt_token = st.session_state.get("id_token") # authorization token
+
+    api_url = "https://xuyzj7f0zd.execute-api.us-east-1.amazonaws.com/prod/add-coi"
+    headers = {
+        "Authorization": cognito_jwt_token,
+        "Content-Type": "application/json"
+    }
+    data = {
+        "email": email,
+        "full_name": full_name,
+        "token_balance": initial_tokens,
+        "token_price": initial_price,
+        "access_on": access_on
+    }
+    return requests.post(api_url, json=data, headers=headers)
+
+def delete_coi(emails):
+    """
+    Deletes a COI from the database.
+
+    Parameters:
+    - email (list of str): Email addresses of the COI to delete.
+    """
+    st.write(f"emails: {emails}")
+    cognito_jwt_token = st.session_state.get("id_token") # authorization token
+    api_url = "https://7893kaawd5.execute-api.us-east-1.amazonaws.com/prod/delete-coi"
+    headers = {
+        "Authorization": cognito_jwt_token,
+        "Content-Type": "application/json"
+    }
+    data = {
+        "emails": emails
+    }
+    return requests.post(api_url, json=data, headers=headers)
+
+@st.cache_data()
+def load_coi_table():
+    try:
+        response = S3_CLIENT.get_object(Bucket=BUCKET_NAME, Key=COI_TABLE_NAME)
+        data = response['Body'].read()
+        return pd.read_parquet(BytesIO(data))
+    except Exception as e:
+        st.error(f"Error loading COI table: {e}")
+        return pd.DataFrame()
+    
+def update_coi_df_on_submit(df, response, coi_table_container):
+    if response.status_code == 200:
+        st.write(response)
+        # resp_json = response.json()
+        # message = resp_json.get("message", "Success")
+        placeholder = st.empty()
+        # placeholder.success(message)
+        # Clear the cached table so the next load reflects the update
+        load_coi_table.clear()
+        coi_df = load_coi_table()
+        coi_table_container.dataframe(coi_df)
+        st.rerun()
+        sleep(4)
+        placeholder.empty()
+    else:
+        st.error(f"Error: {response.status_code} - {response.text}")
+
--- dashboard-admin.py
+++ dashboard-admin.py
@@ -5,9 +5,26 @@ from PIL import Image
 import base64
 from io import BytesIO
 from time import sleep
+import boto3
+from io import BytesIO
+# import py files
 import auth
+import admin_functions as af
+
+
+
 
 st.set_page_config(page_title="Admin Dashboard", layout="wide")
+
+auth.handle_auth()
+
+
+
+
+
+coi_df = af.load_coi_table()
+
+
 # Load the image
 image = Image.open("assets/eureka_logo.jpeg")
 
@@ -22,16 +39,12 @@ st.markdown(f"""
     </div>
 """, unsafe_allow_html=True)
 
-auth.handle_auth()
 
 
 
 
-# Simulated user and token data
-user_data = pd.DataFrame([
-    {"Email": "user1@example.com", "Active": True, "Tokens": 120, "Referral": "ref_001"},
-    {"Email": "user2@example.com", "Active": False, "Tokens": 80, "Referral": "ref_002"},
-])
+
+# simulated data
 token_logs = pd.DataFrame([
     {"User": "user1@example.com", "Action": "Used 20 tokens", "Date": "2024-03-29"},
     {"User": "user2@example.com", "Action": "Refund request", "Date": "2024-03-28"},
@@ -46,77 +59,81 @@ section = st.sidebar.radio("Navigate", [
 
 if section == "COI Management":
     st.header("COI Management")
+    coi_table_container = st.container()
+    coi_table_container.dataframe(coi_df)
 
-    add_coi_cols = st.columns(3)
-    with add_coi_cols[0]:
+    
+    coi_cols1, coi_cols2, coi_cols3 = st.columns(3)
+    with coi_cols1.expander("Add new COI"):
+
+
 
         st.subheader("Add New COI")
         with st.form("add_coi"):
-            full_name = st.text_input("Full Name")
-            email = st.text_input("Email")
+            full_name = st.text_input("Full Name", value=None)
+            email = st.text_input("email", value=None)
             initial_tokens = st.number_input("Initial Token Balance", min_value=0)
             initial_price = st.slider("Initial Token Price", value=10, min_value=1, max_value=100)
             access_on = st.toggle("Access On", value=True)
+
             submitted = st.form_submit_button("Add COI")
 
             if submitted:
-                cognito_jwt_token = st.session_state.get("id_token") # authorization token
+                if not email or not full_name:
+                    # Show an error message and stop execution
+                    st.error("Email and Full Name cannot be empty.")
+                    st.stop()
 
-                if not full_name or not email:
-                    st.error("Full Name and Email are required.")
-                else:
-                    api_url = "https://xuyzj7f0zd.execute-api.us-east-1.amazonaws.com/prod/add-coi"
-                    headers = {
-                        "Authorization": cognito_jwt_token,
-                        "Content-Type": "application/json"
-                    }
-                    data = {
-                        "email": email,
-                        "full_name": full_name,
-                        "token_balance": initial_tokens,
-                        "token_price": initial_price,
-                        "access_on": access_on
-                    }
 
-                    try:
-                        response = requests.post(api_url, json=data, headers=headers)
-                        if response.status_code == 200:
-                            resp_json = response.json()
-                            message = resp_json.get("message", "Success")
-                            placeholder = st.empty()
-                            placeholder.success(message)
-                            sleep(2)
-                            placeholder.empty()
-                        else:
-                            st.error(f"Error: {response.status_code} - {response.text}")
-                    except Exception as e:
-                        st.error(f"Request failed: {e}")
+                response = af.add_new_coi(full_name, email, initial_tokens, initial_price, access_on)
+                af.update_coi_df_on_submit(coi_df, response, coi_table_container)
 
-    st.subheader("All Users")
-    st.dataframe(user_data)
 
-    st.subheader("Modify User")
-    selected_email = st.selectbox("Select User", user_data["Email"])
-    col1, col2, col3 = st.columns(3)
+    with coi_cols2.expander("Delete COI"):
+        st.warning("to delete COI: add email -> ENTER -> add another email ... -> Delete COI")
+        with st.form("delete_coi"):
+            emails = st.text_area("add email, click enter, add another one if needed", placeholder=None)
+            submitted = st.form_submit_button("Delete COI")
+            if submitted:
+                if not emails:
+                    st.error("Emails cannot be empty.")
+                    st.stop()
+                # try:
+                response = af.delete_coi(emails)
+                af.update_coi_df_on_submit(coi_df, response, coi_table_container)
 
-    with col1:
-        st.checkbox("Active", value=user_data[user_data["Email"] == selected_email]["Active"].iloc[0])
-    with col2:
-        st.number_input("Token Balance", value=int(user_data[user_data["Email"] == selected_email]["Tokens"].iloc[0]))
-    with col3:
-        st.text_input("Referral Account", value=user_data[user_data["Email"] == selected_email]["Referral"].iloc[0])
+                # except Exception as e:
+                #     st.error(f"Request failed: {e}")
+            
+                
+            
+        
 
-    st.button("Save Changes")
+    # st.subheader("All Users")
+    # st.dataframe(coi_df)
 
-    st.subheader("Referral Performance")
-    st.text("Coming soon: Referral performance dashboard.")
+    # st.subheader("Modify User")
+    # selected_email = st.selectbox("Select User", coi_df["email"])
+    # col1, col2, col3 = st.columns(3)
+
+    # with col1:
+    #     st.checkbox("Active", value=coi_df[coi_df["email"] == selected_email]["Active"].iloc[0])
+    # with col2:
+    #     st.number_input("Token Balance", value=int(coi_df[coi_df["email"] == selected_email]["Tokens"].iloc[0]))
+    # with col3:
+    #     st.text_input("Referral Account", value=coi_df[coi_df["email"] == selected_email]["Referral"].iloc[0])
+
+    # st.button("Save Changes")
+
+    # st.subheader("Referral Performance")
+    # st.text("Coming soon: Referral performance dashboard.")
 
 elif section == "Token Management":
     st.header("Token Management")
 
     st.subheader("Edit Tokens Manually")
     with st.form("edit_tokens"):
-        email = st.selectbox("Select User", user_data["Email"], key="edit_tokens_user")
+        email = st.selectbox("Select User", coi_df["email"], key="edit_tokens_user")
         tokens = st.number_input("Adjust Token Count", value=0)
         st.form_submit_button("Update Tokens")
 
@@ -124,7 +141,7 @@ elif section == "Token Management":
     st.dataframe(token_logs)
 
     st.subheader("Refund Requests")
-    refund_user = st.selectbox("Select User Requesting Refund", user_data["Email"], key="refund_user")
+    refund_user = st.selectbox("Select User Requesting Refund", coi_df["email"], key="refund_user")
     st.text_area("Reason for Refund")
     col1, col2 = st.columns(2)
     with col1:
